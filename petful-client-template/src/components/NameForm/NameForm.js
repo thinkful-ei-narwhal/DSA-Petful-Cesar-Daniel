c1b3a722788882b42e3ad6fb6049f28a078eb2db
Cesar Morales <cesarlenin2@live.com>
2020-06-19 11:48:19 -0700

    nameform layout

--- petful-client-template/src/components/NameForm/NameForm.js
+++ petful-client-template/src/components/NameForm/NameForm.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
+import ValidationError from '../ValidationError'
 
 export class NameForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: {
+        value: '',
+        touched: false,
+      }
+    };
+  }
+
+  validateName() {
+    const name = this.state.name.value;
+    if (!name) {
+      return 'name is required';
+    } 
+  }
+  updateName(name) {
+    this.setState({ name: { value: name, touched: true } });
+  }
+
+
   render() {
+    const nameError = this.validateName();
     return (
-      <div>
-        
-      </div>
+      <form>
+        {this.state.name.touched && <ValidationError message={nameError} />}
+        <label htmlFor='name'>name:</label>
+        <input type= 'text' name='name' id= 'name' 
+        onChange={(e) => this.updateName(e.target.value)}>
+        </input>
+    
+        <button type= 'submit' value='Submit' disabled={this.validateName()}>
+          Submit
+        </button>
+      </form>
     )
   }
 }
